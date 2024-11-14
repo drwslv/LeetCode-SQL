c75c8922b46ff342564529f89ad9f3d1a1d496b5
drwslv <aasilva@gmail.com>
2024-11-13 21:48:49 -0800

    (2) Ex2 1757

--- Macbook M2 Local.session.sql
+++ Macbook M2 Local.session.sql
@@ -154,5 +154,23 @@ insert into World (name, continent, area, population, gdp) values ('Angola', 'Af
 SELECT name, population, area from World
     WHERE area >= 3000000 OR population >= 25000000;
 
+/* EXAMPLE 2: 1757. Recyclable and Low Fat Products
 
+Write a solution to find the ids of products that are both low fat and recyclable.
 
+Return the result table in any order.
+
+The result format is in the following example. */
+
+Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
+Truncate table Products;
+insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
+insert into Products (product_id, low_fats, recyclable) values ('1', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('2', 'N', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('3', 'Y', 'Y');
+insert into Products (product_id, low_fats, recyclable) values ('4', 'N', 'N');
+
+select * from Products;
+
+select product_id from Products
+    where low_fats='Y' and recyclable='Y';
