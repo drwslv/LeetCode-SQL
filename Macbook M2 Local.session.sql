79a06802482aea2b7e02d39f4cdc402bf50de86f
drwslv <aasilva@gmail.com>
2024-11-26 11:44:17 -0800

    (2) Ex9 511

--- Macbook M2 Local.session.sql
+++ Macbook M2 Local.session.sql
@@ -546,3 +546,30 @@ from Followers
 group by user_id
 order by user_id asc;
 
+
+/* EXAMPLE 9: 511. Game Play Analysis I
+
+Write a solution to find the first login date for each player.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+*/
+
+drop table if exists Activity;
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int);
+Truncate table Activity;
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6');
+insert into Activity (player_id, device_id, event_date, games_played) values ('2', '3', '2017-06-25', '1');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5');
+
+select * from Activity;
+
+select player_id, min(event_date) as first_login
+from Activity
+group by player_id
+order by player_id asc;
+
