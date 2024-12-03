9f2a640c2ec7e5cf88111a32ef7c218c9909d2a7
drwslv <aasilva@gmail.com>
2024-12-02 17:29:12 -0800

    (3) Ex1 1581

--- Chapter 3 SQL Relationship.sql
+++ Chapter 3 SQL Relationship.sql
@@ -49,8 +49,58 @@ FROM `new_schema`.`users`
 INNER JOIN `new_schema`.`orders` ON `users`.`id` = `orders`.`user_id`;
 
 
-/* EXAMPLE 1: 
+/* EXAMPLE 1: 1581. Customer Who Visited but Did Not Make Any Transactions
+
+Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+
+Return the result table sorted in any order.
+
+The result format is in the following example.
 
 */
 
+Create table If Not Exists Visits(visit_id int, customer_id int);
+Create table If Not Exists Transactions(transaction_id int, visit_id int, amount int);
+Truncate table Visits;
+insert into Visits (visit_id, customer_id) values ('1', '23');
+insert into Visits (visit_id, customer_id) values ('2', '9');
+insert into Visits (visit_id, customer_id) values ('4', '30');
+insert into Visits (visit_id, customer_id) values ('5', '54');
+insert into Visits (visit_id, customer_id) values ('6', '96');
+insert into Visits (visit_id, customer_id) values ('7', '54');
+insert into Visits (visit_id, customer_id) values ('8', '54');
+Truncate table Transactions;
+insert into Transactions (transaction_id, visit_id, amount) values ('2', '5', '310');
+insert into Transactions (transaction_id, visit_id, amount) values ('3', '5', '300');
+insert into Transactions (transaction_id, visit_id, amount) values ('9', '5', '200');
+insert into Transactions (transaction_id, visit_id, amount) values ('12', '1', '910');
+insert into Transactions (transaction_id, visit_id, amount) values ('13', '2', '970');
+
+select * from Visits;
+select * from Transactions;
+
+select customer_id, count(*) as count_no_trans
+from (
+    select Visits.visit_id as visit_id, customer_id, transaction_id
+    from Visits
+    left join Transactions on Visits.visit_id = Transactions.visit_id
+) as subquery
+where transaction_id is null
+group by customer_id;
+
+-- OR --
+
+SELECT 
+  customer_id, COUNT(visit_id) AS count_no_trans 
+FROM 
+  Visits 
+WHERE 
+  visit_id NOT IN (
+    SELECT 
+      visit_id 
+    FROM 
+      Transactions
+  ) 
+GROUP BY 
+  customer_id
 
