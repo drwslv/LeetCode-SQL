64f9a71e208497e124bc21f7be0afc5d6f42cbd9
drwslv <aasilva@gmail.com>
2024-12-13 15:46:56 -0800

    (3) Ex3 1158 alt

--- Chapter 3 SQL Relationship.sql
+++ Chapter 3 SQL Relationship.sql
@@ -185,4 +185,17 @@ from (
 ) as q2
 group by q2.user_id, q2.join_date;
 
+-- OR --
+
+select u.user_id, u.join_date, count(o.item_id) as orders_in_2019
+from Users u
+    left join Orders o on u.user_id = o.buyer_id
+    and year(o.order_date) = 2019
+group by u.user_id, u.join_date;
+
+
+
+
+
+
 
