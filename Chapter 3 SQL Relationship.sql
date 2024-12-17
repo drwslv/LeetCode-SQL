7a4bb09aebb497368be2a8ad1626146472ea1d01
drwslv <aasilva@gmail.com>
2024-12-16 16:03:28 -0800

    (3) Ex6 586

--- Chapter 3 SQL Relationship.sql
+++ Chapter 3 SQL Relationship.sql
@@ -353,3 +353,41 @@ where c.id not in (
 );
 
 
+/* EXAMPLE 6: 586. Customer Placing the Largest Number of Orders
+
+Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+
+The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+
+The result format is in the following example.
+
+*/
+
+drop table if exists orders;
+Create table If Not Exists orders (order_number int, customer_number int);
+insert into orders (order_number, customer_number) values ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '3');
+
+select * from orders;
+
+select c.customer_number
+from (
+    select customer_number, count(order_number) as order_count
+    from orders
+    group by customer_number
+    order by order_count desc
+    limit 1
+) as c;
+
+-- OR --
+
+select customer_number
+from orders
+group by customer_number
+order by count(*) desc
+limit 1;
+
+
+
