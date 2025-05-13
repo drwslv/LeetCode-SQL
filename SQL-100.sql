4d78b45828a755cb5a911593098437e354864a90
drwslv <aasilva@gmail.com>
2025-05-13 14:09:40 -0700

    SQL 100 May 13, 1-5

--- SQL-100.sql
+++ SQL-100.sql
@@ -5,6 +5,7 @@
 Write a solution to find the ids of products that are both low fat and recyclable.
 */
 
+Drop table if exists Products;
 Create table If Not Exists Products (product_id int, low_fats ENUM('Y', 'N'), recyclable ENUM('Y','N'));
 Truncate table Products;
 insert into Products (product_id, low_fats, recyclable) values ('0', 'Y', 'N');
@@ -26,6 +27,7 @@ Find the names of the customer that are not referred by the customer with id = 2
 Return the result table in any order.
 */
 
+Drop table if exists Customer;
 Create table If Not Exists Customer (id int, name varchar(25), referee_id int);
 Truncate table Customer;
 insert into Customer (id, name, referee_id) values ('1', 'Will', NULL);
@@ -48,6 +50,7 @@ Write a solution to find all dates' id with higher temperatures compared to its 
 Return the result table in any order.
 */
 
+Drop table if exists Weather;
 Create table If Not Exists Weather (id int, recordDate date, temperature int);
 Truncate table Weather;
 insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10');
@@ -76,6 +79,8 @@ If the address of a personId is not present in the Address table, report null in
 Return the result table in any order.
 */
 
+Drop table if exists Person;
+Drop table if exists Address;
 Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255));
 Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255));
 Truncate table Person;
@@ -101,6 +106,7 @@ Write a solution to find all the authors that viewed at least one of their own a
 Return the result table sorted by id in ascending order.
 */
 
+Drop table if exists Views;
 Create table If Not Exists Views (article_id int, author_id int, viewer_id int, view_date date);
 Truncate table Views;
 insert into Views (article_id, author_id, viewer_id, view_date) values ('1', '3', '5', '2019-08-01');
@@ -128,6 +134,7 @@ Write a solution to find the employees who earn more than their managers.
 Return the result table in any order.
 */
 
+Drop table if exists Employee;
 Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
 Truncate table Employee;
 insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
@@ -150,25 +157,120 @@ Write a solution to find the second highest distinct salary from the Employee ta
 If there is no second highest salary, return null (return None in Pandas).
 */
 
+Drop table if exists Employee;
 Create table If Not Exists Employee (id int, salary int);
 Truncate table Employee;
 insert into Employee (id, salary) values ('1', '100');
 insert into Employee (id, salary) values ('2', '200');
 insert into Employee (id, salary) values ('3', '300');
 
-SELECT
-    *
-FROM
-    Employee;
+-- Wrapping in a SELECT statement guarantees it will return NULL if no second element found
+SELECT (
+    SELECT
+        DISTINCT salary
+    FROM
+        Employee
+    ORDER BY
+        salary DESC
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
+
+
+/* 570. Managers with at Least 5 Direct Reports [M]
+Write a solution to find managers with at least five direct reports.
+Return the result table in any order.
+*/
+
+Drop table if exists Employee;
+Create table If Not Exists Employee (id int, name varchar(255), department varchar(255), managerId int);
+Truncate table Employee;
+insert into Employee (id, name, department, managerId) values ('101', 'John', 'A', NULL);
+insert into Employee (id, name, department, managerId) values ('102', 'Dan', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('103', 'James', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('104', 'Amy', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('105', 'Anne', 'A', '101');
+insert into Employee (id, name, department, managerId) values ('106', 'Ron', 'B', '101');
+
+SELECT e2.name
+FROM Employee e1
+JOIN Employee e2 ON e1.managerId = e2.id
+GROUP BY e2.id, e2.name
+HAVING COUNT(e1.id) >= 5;
+
+
+/* 1661. Average Time of Process per Machine [E]
+There is a factory website that has several machines each running the same number of processes.
+Write a solution to find the average time each machine takes to complete a process.
+
+The time to complete a process is the 'end' timestamp minus the 'start' timestamp.
+The average time is calculated by the total time to complete every process on the machine divided
+by the number of processes that were run.
+
+The resulting table should have the machine_id along with the average time as processing_time,
+which should be rounded to 3 decimal places.
+
+Return the result table in any order.
+*/
+
+Drop table if exists Activity;
+Create table If Not Exists Activity (machine_id int, process_id int, activity_type ENUM('start', 'end'), timestamp float);
+Truncate table Activity;
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('0', '0', 'start', '0.712');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('0', '0', 'end', '1.52');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('0', '1', 'start', '3.14');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('0', '1', 'end', '4.12');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('1', '0', 'start', '0.55');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('1', '0', 'end', '1.55');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('1', '1', 'start', '0.43');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('1', '1', 'end', '1.42');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '0', 'start', '4.1');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '0', 'end', '4.512');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '1', 'start', '2.5');
+insert into Activity (machine_id, process_id, activity_type, timestamp) values ('2', '1', 'end', '5');
+
+SELECT machine_id, ROUND(SUM(time_complete)/COUNT(process_id), 3) AS processing_time
+FROM (
+    SELECT T1.machine_id, T1.process_id, (T2.time_end - T1.time_start) AS time_complete
+    FROM (
+        SELECT machine_id, process_id, timestamp AS time_start
+        FROM Activity a1
+        WHERE activity_type = 'start' -- machine_id, process_id, time_start
+    ) AS T1
+    JOIN (
+        SELECT machine_id, process_id, timestamp AS time_end
+        FROM Activity a2
+        WHERE activity_type = 'end' -- machine_id, process_id, time_end
+    ) AS T2 ON T1.machine_id = T2.machine_id AND T1.process_id = T2.process_id
+) AS T3
+GROUP BY machine_id;
+
+
+/* 595. Big Countries [E]
+A country is big if:
+-it has an area of at least three million (i.e., 3000000 km2), or
+-it has a population of at least twenty-five million (i.e., 25000000).
+
+Write a solution to find the name, population, and area of the big countries.
+Return the result table in any order.
+*/
+
+Drop table if exists World;
+Create table If Not Exists World (name varchar(255), continent varchar(255), area int, population int, gdp bigint);
+Truncate table World;
+insert into World (name, continent, area, population, gdp) values ('Afghanistan', 'Asia', '652230', '25500100', '20343000000');
+insert into World (name, continent, area, population, gdp) values ('Albania', 'Europe', '28748', '2831741', '12960000000');
+insert into World (name, continent, area, population, gdp) values ('Algeria', 'Africa', '2381741', '37100000', '188681000000');
+insert into World (name, continent, area, population, gdp) values ('Andorra', 'Europe', '468', '78115', '3712000000');
+insert into World (name, continent, area, population, gdp) values ('Angola', 'Africa', '1246700', '20609294', '100990000000');
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
 
 
 
-SELECT
-    DISTINCT salary
-FROM
-    Employee
-ORDER BY
-    salary
+
+
 
 
 
