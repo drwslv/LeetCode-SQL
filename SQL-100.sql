94bead5c370b712d3f3ac303ee55de2fd45f35d9
drwslv <aasilva@gmail.com>
2025-05-16 11:11:45 -0700

    577

--- SQL-100.sql
+++ SQL-100.sql
@@ -747,3 +747,37 @@ FROM (
 
 
 
+-- May 16 --
+
+
+
+/* 577. Employee Bonus [E]
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+Return the result table in any order.
+*/
+
+Drop table if exists Employee;
+Drop table if exists Bonus;
+Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int);
+Create table If Not Exists Bonus (empId int, bonus int);
+Truncate table Employee;
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', NULL, '4000');
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000');
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000');
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000');
+Truncate table Bonus;
+insert into Bonus (empId, bonus) values ('2', '500');
+insert into Bonus (empId, bonus) values ('4', '2000');
+
+SELECT *
+FROM Employee;
+
+SELECT *
+FROM Bonus;
+
+SELECT E.name as name, B.bonus as bonus
+FROM Employee E
+LEFT JOIN Bonus B
+ON E.empId = B.empId
+WHERE B.bonus < 1000 OR B.bonus IS NULL;
+
