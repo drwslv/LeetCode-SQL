30fbb0fb7610e169fb0502a3f66d7eed296d7398
drwslv <aasilva@gmail.com>
2025-05-19 21:17:36 -0700

    1596

--- SQL-100.sql
+++ SQL-100.sql
@@ -1698,5 +1698,66 @@ AND manager_id NOT IN (
 ORDER BY employee_id;
 
 
+/* 1596. The Most Frequently Ordered Products for Each Customer [M]
+Write a solution to find the most frequently ordered product(s) for each customer.
+The result table should have the product_id and product_name for each customer_id who ordered at least one order.
+Return the result table in any order.
+*/
+
+Drop table if exists Customers;
+Drop table if exists Orders;
+Drop table if exists Products;
+Create table If Not Exists Customers (customer_id int, name varchar(10));
+Create table If Not Exists Orders (order_id int, order_date date, customer_id int, product_id int);
+Create table If Not Exists Products (product_id int, product_name varchar(20), price int);
+Truncate table Customers;
+insert into Customers (customer_id, name) values ('1', 'Alice');
+insert into Customers (customer_id, name) values ('2', 'Bob');
+insert into Customers (customer_id, name) values ('3', 'Tom');
+insert into Customers (customer_id, name) values ('4', 'Jerry');
+insert into Customers (customer_id, name) values ('5', 'John');
+Truncate table Orders;
+insert into Orders (order_id, order_date, customer_id, product_id) values ('1', '2020-07-31', '1', '1');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('2', '2020-7-30', '2', '2');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('3', '2020-08-29', '3', '3');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('4', '2020-07-29', '4', '1');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('5', '2020-06-10', '1', '2');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('6', '2020-08-01', '2', '1');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('7', '2020-08-01', '3', '3');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('8', '2020-08-03', '1', '2');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('9', '2020-08-07', '2', '3');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('10', '2020-07-15', '1', '2');
+Truncate table Products;
+insert into Products (product_id, product_name, price) values ('1', 'keyboard', '120');
+insert into Products (product_id, product_name, price) values ('2', 'mouse', '80');
+insert into Products (product_id, product_name, price) values ('3', 'screen', '600');
+insert into Products (product_id, product_name, price) values ('4', 'hard disk', '450');
+
+SELECT *
+FROM Customers;
+
+SELECT *
+FROM Orders;
+
+SELECT *
+FROM Products;
+
+WITH PC AS ( -- unique customer_id, product_id with purchase_count
+    SELECT customer_id, product_id, COUNT(*) AS purchase_count
+    FROM Orders
+    GROUP BY customer_id, product_id
+)
+SELECT PC1.customer_id AS customer_id, PC1.product_id AS product_id, P.product_name AS product_name
+FROM PC PC1
+INNER JOIN ( -- max product count number per customer_id
+    SELECT customer_id, MAX(purchase_count) AS pc_max
+    FROM PC
+    GROUP BY customer_id
+) AS PC2
+ON PC1.customer_id = PC2.customer_id AND PC1.purchase_count = PC2.pc_max
+INNER JOIN -- add product_name
+    Products P
+ON PC1.product_id = P.product_id
+ORDER BY PC1.customer_id;
 
 
