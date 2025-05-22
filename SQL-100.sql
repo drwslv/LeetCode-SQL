2b761e6d85089c7376b2f269b59678600bf722a4
drwslv <aasilva@gmail.com>
2025-05-22 13:53:18 -0700

    2356

--- SQL-100.sql
+++ SQL-100.sql
@@ -2021,5 +2021,27 @@ SET sex = CASE
 END;
 
 
+/* 2356. Number of Unique Subjects Taught by Each Teacher [E]
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+Return the result table in any order.
+*/
+
+Drop table if exists Teacher;
+Create table If Not Exists Teacher (teacher_id int, subject_id int, dept_id int);
+Truncate table Teacher;
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '3');
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '2', '4');
+insert into Teacher (teacher_id, subject_id, dept_id) values ('1', '3', '3');
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '1', '1');
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '2', '1');
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '3', '1');
+insert into Teacher (teacher_id, subject_id, dept_id) values ('2', '4', '1');
+
+SELECT *
+FROM Teacher;
+
+SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
+FROM Teacher
+GROUP BY teacher_id;
 
 
