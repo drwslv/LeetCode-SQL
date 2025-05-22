2ef5e242d3b631f1739f8948df9cff46d6b6377a
drwslv <aasilva@gmail.com>
2025-05-21 22:55:02 -0700

    633

--- SQL-100.sql
+++ SQL-100.sql
@@ -1947,3 +1947,47 @@ FROM (
 ) AS T
 ORDER BY order_count DESC
 LIMIT 1
+
+
+/* 633. Percentage of Users Attended a Contest [E]
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+*/
+
+Drop table if exists Users;
+Drop table if exists Register;
+Create table If Not Exists Users (user_id int, user_name varchar(20));
+Create table If Not Exists Register (contest_id int, user_id int);
+Truncate table Users;
+insert into Users (user_id, user_name) values ('6', 'Alice');
+insert into Users (user_id, user_name) values ('2', 'Bob');
+insert into Users (user_id, user_name) values ('7', 'Alex');
+Truncate table Register;
+insert into Register (contest_id, user_id) values ('215', '6');
+insert into Register (contest_id, user_id) values ('209', '2');
+insert into Register (contest_id, user_id) values ('208', '2');
+insert into Register (contest_id, user_id) values ('210', '6');
+insert into Register (contest_id, user_id) values ('208', '6');
+insert into Register (contest_id, user_id) values ('209', '7');
+insert into Register (contest_id, user_id) values ('209', '6');
+insert into Register (contest_id, user_id) values ('215', '7');
+insert into Register (contest_id, user_id) values ('208', '7');
+insert into Register (contest_id, user_id) values ('210', '2');
+insert into Register (contest_id, user_id) values ('207', '2');
+insert into Register (contest_id, user_id) values ('210', '7');
+
+SELECT *
+FROM Users;
+
+SELECT *
+FROM Register;
+
+SELECT contest_id,
+    (
+        ROUND(100*COUNT(*)/(SELECT COUNT(user_id) FROM Users), 2)
+    ) AS percentage
+FROM Register R
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC
+
+
