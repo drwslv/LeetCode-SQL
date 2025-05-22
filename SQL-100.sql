154c4fae13d31dd0010eb21531d41e2e8353b0df
drwslv <aasilva@gmail.com>
2025-05-21 22:19:35 -0700

    586

--- SQL-100.sql
+++ SQL-100.sql
@@ -1923,4 +1923,27 @@ WHERE primary_flag = 'Y'
 ORDER BY employee_id
 
 
+/* 586. Customer Placing the Largest Number of Orders [E]
+Write a solution to find the customer_number for the customer who has placed the largest number of orders.
+The test cases are generated so that exactly one customer will have placed more orders than any other customer.
+*/
 
+Drop table if exists orders;
+Create table If Not Exists orders (order_number int, customer_number int);
+Truncate table orders;
+insert into orders (order_number, customer_number) values ('1', '1');
+insert into orders (order_number, customer_number) values ('2', '2');
+insert into orders (order_number, customer_number) values ('3', '3');
+insert into orders (order_number, customer_number) values ('4', '3');
+
+SELECT *
+FROM orders;
+
+SELECT T.customer_number AS customer_number
+FROM (
+    SELECT customer_number, COUNT(*) AS order_count
+    FROM orders
+    GROUP BY customer_number
+) AS T
+ORDER BY order_count DESC
+LIMIT 1
