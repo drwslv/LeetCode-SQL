4f9be13a80c692a9a9bb99ecf532b939e720b5ca
drwslv <aasilva@gmail.com>
2025-09-02 11:27:35 -0700

    597 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -404,19 +404,70 @@ SELECT trans_month AS month, country,
 FROM AllData
 GROUP BY trans_month, country;
 
-
-
-
-
-
-
-
-
-
 Drop table if exists Transactions;
 Drop table if exists Chargebacks;
 
 
+
+/* 597. Friend Requests I: Overall Acceptance Rate [E]
+Find the overall acceptance rate of requests, which is the number of acceptance divided by the number of requests.
+Return the answer rounded to 2 decimals places.
+
+Note that:
+
+* The accepted requests are not necessarily from the table friend_request. In this case, Count the total accepted requests
+(no matter whether they are in the original requests), and divide it by the number of requests to get the acceptance rate.
+
+* It is possible that a sender sends multiple requests to the same receiver, and a request could be accepted more than once.
+In this case, the ‘duplicated’ requests or acceptances are only counted once.
+
+* If there are no requests at all, you should return 0.00 as the accept_rate.
+*/
+
+Drop table if exists FriendRequest;
+Drop table if exists RequestAccepted;
+Create table If Not Exists FriendRequest (sender_id int, send_to_id int, request_date date);
+Create table If Not Exists RequestAccepted (requester_id int, accepter_id int, accept_date date);
+Truncate table FriendRequest;
+insert into FriendRequest (sender_id, send_to_id, request_date) values ('1', '2', '2016/06/01');
+insert into FriendRequest (sender_id, send_to_id, request_date) values ('1', '3', '2016/06/01');
+insert into FriendRequest (sender_id, send_to_id, request_date) values ('1', '4', '2016/06/01');
+insert into FriendRequest (sender_id, send_to_id, request_date) values ('2', '3', '2016/06/02');
+insert into FriendRequest (sender_id, send_to_id, request_date) values ('3', '4', '2016/06/09');
+Truncate table RequestAccepted;
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '2', '2016/06/03');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '3', '2016/06/08');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('2', '3', '2016/06/08');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('3', '4', '2016/06/09');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('3', '4', '2016/06/10');
+
+SELECT *
+FROM FriendRequest;
+
+SELECT *
+FROM RequestAccepted;
+
+SELECT IFNULL(ROUND(
+(
+    SELECT COUNT(*) AS ac_count
+    FROM (
+        SELECT requester_id, accepter_id
+        FROM RequestAccepted
+        GROUP BY requester_id, accepter_id
+    ) AS RA
+) / (
+    SELECT COUNT(*) AS rq_count
+    FROM (
+        SELECT sender_id, send_to_id
+        FROM FriendRequest
+        GROUP BY sender_id, send_to_id
+    ) AS FR
+)
+,2),0) AS accept_rate;
+
+
+Drop table if exists FriendRequest;
+Drop table if exists RequestAccepted;
 
 
 
