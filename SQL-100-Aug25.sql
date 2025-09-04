b7ed2517d279dfa0044c1cd1a78c9a7dc0c0e008
drwslv <aasilva@gmail.com>
2025-09-04 10:30:50 -0700

    1050 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -602,3 +602,31 @@ Drop table if exists Failed;
 Drop table if exists Succeeded;
 
 
+/* 1050. Actors and Directors Who Cooperated At Least Three Times [E]
+Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+
+Return the result table in any order.
+*/
+
+Drop table if exists ActorDirector;
+Create table If Not Exists ActorDirector (actor_id int, director_id int, timestamp int);
+Truncate table ActorDirector;
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '0');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '1');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '1', '2');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '3');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('1', '2', '4');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '5');
+insert into ActorDirector (actor_id, director_id, timestamp) values ('2', '1', '6');
+
+SELECT *
+FROM ActorDirector;
+
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
+
+Drop table if exists ActorDirector;
+
+
