f52055748379466789a8d6f63cb18df60cc79097
drwslv <aasilva@gmail.com>
2025-09-06 08:58:57 -0700

    1393 [M]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -815,4 +815,43 @@ SELECT (
 
 Drop table if exists MyNumbers;
 
+/* 1393. Capital Gain/Loss [M]
+Write a solution to report the Capital gain/loss for each stock.
+
+The Capital gain/loss of a stock is the total gain or loss after buying and selling the stock one or many times.
+
+Return the result table in any order.
+*/
+
+Drop table if exists Stocks;
+Create Table If Not Exists Stocks (stock_name varchar(15), operation ENUM('Sell', 'Buy'), operation_day int, price int);
+Truncate table Stocks;
+insert into Stocks (stock_name, operation, operation_day, price) values ('Leetcode', 'Buy', '1', '1000');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Buy', '2', '10');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Leetcode', 'Sell', '5', '9000');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Handbags', 'Buy', '17', '30000');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Sell', '3', '1010');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Buy', '4', '1000');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Sell', '5', '500');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Buy', '6', '1000');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Handbags', 'Sell', '29', '7000');
+insert into Stocks (stock_name, operation, operation_day, price) values ('Corona Masks', 'Sell', '10', '10000');
+
+SELECT *
+FROM Stocks;
+
+WITH CTE AS (
+    SELECT stock_name,
+        operation,
+        CASE operation
+            WHEN 'Buy' THEN -price
+            WHEN 'Sell' THEN price
+        END AS price2
+    FROM Stocks
+)
+SELECT stock_name, SUM(price2) AS capital_gain_loss
+FROM CTE
+GROUP BY stock_name;
+
+Drop table if exists Stocks;
 
