a2ef3c8f403711affca3533c48b728601438985c
drwslv <aasilva@gmail.com>
2025-09-07 10:45:16 -0700

    1890 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -954,3 +954,35 @@ WHERE ordinal IN (FLOOR((cnt + 1) / 2), FLOOR((cnt + 2) / 2));
 
 Drop table if exists Employee;
 
+
+/* 1890. The Latest Login in 2020 [E]
+Write a solution to report the latest login for all users in the year 2020. Do not include the users who did not login in 2020.
+
+Return the result table in any order.
+*/
+
+Drop table if exists Logins;
+Create table If Not Exists Logins (user_id int, time_stamp datetime);
+Truncate table Logins;
+insert into Logins (user_id, time_stamp) values ('6', '2020-06-30 15:06:07');
+insert into Logins (user_id, time_stamp) values ('6', '2021-04-21 14:06:06');
+insert into Logins (user_id, time_stamp) values ('6', '2019-03-07 00:18:15');
+insert into Logins (user_id, time_stamp) values ('8', '2020-02-01 05:10:53');
+insert into Logins (user_id, time_stamp) values ('8', '2020-12-30 00:46:50');
+insert into Logins (user_id, time_stamp) values ('2', '2020-01-16 02:49:50');
+insert into Logins (user_id, time_stamp) values ('2', '2019-08-25 07:59:08');
+insert into Logins (user_id, time_stamp) values ('14', '2019-07-14 09:00:00');
+insert into Logins (user_id, time_stamp) values ('14', '2021-01-06 11:59:59');
+
+SELECT *
+FROM Logins;
+
+SELECT user_id, MAX(time_stamp) AS last_stamp
+FROM Logins
+WHERE YEAR(time_stamp) = '2020'
+GROUP BY user_id;
+
+
+Drop table if exists Logins;
+
+
