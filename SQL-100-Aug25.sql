89225aa306c0b347b7e7a778a0c506925d0ee90f
drwslv <aasilva@gmail.com>
2025-09-08 08:44:31 -0700

    534 [M]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -1078,4 +1078,31 @@ WHERE (med1 >= lower_bound AND med1 <= upper_bound)
 Drop table if exists Numbers;
 
 
+/* 534. Game Play Analysis III [M]
+Write a solution to report for each player and date, how many games played so far by the player.
+That is, the total number of games played by the player until that date. Check the example for clarity.
+
+Return the result table in any order.
+*/
+
+Drop table if exists Activity;
+Create table If Not Exists Activity (player_id int, device_id int, event_date date, games_played int);
+Truncate table Activity;
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-03-01', '5');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '2', '2016-05-02', '6');
+insert into Activity (player_id, device_id, event_date, games_played) values ('1', '3', '2017-06-25', '1');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '1', '2016-03-02', '0');
+insert into Activity (player_id, device_id, event_date, games_played) values ('3', '4', '2018-07-03', '5');
+
+SELECT *
+FROM Activity;
+
+SELECT player_id, event_date,
+    SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) AS games_played_so_far
+FROM Activity;
+
+Drop table if exists Activity;
+
+
+
 
