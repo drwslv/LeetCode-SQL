3046d9bc3fdfadd72d68404c5107cfd47d628415
drwslv <aasilva@gmail.com>
2025-09-09 09:14:14 -0700

    596 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -1159,5 +1159,31 @@ ORDER BY transaction_date;
 Drop table if exists transactions;
 
 
+/* 596. Classes With at Least 5 Students [E]
+Write a solution to find all the classes that have at least five students.
 
+Return the result table in any order.
+*/
 
+Drop table if exists Courses;
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+Truncate table Courses;
+insert into Courses (student, class) values ('A', 'Math');
+insert into Courses (student, class) values ('B', 'English');
+insert into Courses (student, class) values ('C', 'Math');
+insert into Courses (student, class) values ('D', 'Biology');
+insert into Courses (student, class) values ('E', 'Math');
+insert into Courses (student, class) values ('F', 'Computer');
+insert into Courses (student, class) values ('G', 'Math');
+insert into Courses (student, class) values ('H', 'Math');
+insert into Courses (student, class) values ('I', 'Math');
+
+SELECT *
+FROM Courses;
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(*) >= 5;
+
+Drop table if exists Courses;
