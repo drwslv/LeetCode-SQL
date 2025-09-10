6f2f06d7bbe63a167cde385ff82344fd6075cd82
drwslv <aasilva@gmail.com>
2025-09-10 07:25:54 -0700

    1484 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -1358,3 +1358,35 @@ WHERE balance > 10000;
 
 Drop table if exists Users;
 Drop table if exists Transactions;
+
+
+/* 1484. Group Sold Products By The Date [E]
+Write a solution to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+*/
+
+Drop table if exists Activities;
+Create table If Not Exists Activities (sell_date date, product varchar(20));
+Truncate table Activities;
+insert into Activities (sell_date, product) values ('2020-05-30', 'Headphone');
+insert into Activities (sell_date, product) values ('2020-06-01', 'Pencil');
+insert into Activities (sell_date, product) values ('2020-06-02', 'Mask');
+insert into Activities (sell_date, product) values ('2020-05-30', 'Basketball');
+insert into Activities (sell_date, product) values ('2020-06-01', 'Bible');
+insert into Activities (sell_date, product) values ('2020-06-02', 'Mask');
+insert into Activities (sell_date, product) values ('2020-05-30', 'T-Shirt');
+
+SELECT *
+FROM Activities;
+
+SELECT sell_date, COUNT(DISTINCT product) AS num_sold, GROUP_CONCAT(DISTINCT product) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;
+
+
+
+Drop table if exists Activities;
