20eba565847b9bd0fbe5595dc0e3b384327eb329
drwslv <aasilva@gmail.com>
2025-09-10 07:17:05 -0700

    1587 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -1321,3 +1321,40 @@ ORDER BY id ASC , month DESC
 
 Drop table if exists Employee;
 
+
+/* 1587. Bank Account Summary II [E]
+Write a solution to report the name and balance of users with a balance higher than 10000.
+The balance of an account is equal to the sum of the amounts of all transactions involving that account.
+
+Return the result table in any order.
+*/
+
+Drop table if exists Users;
+Drop table if exists Transactions;
+Create table If Not Exists Users (account int, name varchar(20));
+Create table If Not Exists Transactions (trans_id int, account int, amount int, transacted_on date);
+Truncate table Users;
+insert into Users (account, name) values ('900001', 'Alice');
+insert into Users (account, name) values ('900002', 'Bob');
+insert into Users (account, name) values ('900003', 'Charlie');
+Truncate table Transactions;
+insert into Transactions (trans_id, account, amount, transacted_on) values ('1', '900001', '7000', '2020-08-01');
+insert into Transactions (trans_id, account, amount, transacted_on) values ('2', '900001', '7000', '2020-09-01');
+insert into Transactions (trans_id, account, amount, transacted_on) values ('3', '900001', '-3000', '2020-09-02');
+insert into Transactions (trans_id, account, amount, transacted_on) values ('4', '900002', '1000', '2020-09-12');
+insert into Transactions (trans_id, account, amount, transacted_on) values ('5', '900003', '6000', '2020-08-07');
+insert into Transactions (trans_id, account, amount, transacted_on) values ('6', '900003', '6000', '2020-09-07');
+insert into Transactions (trans_id, account, amount, transacted_on) values ('7', '900003', '-4000', '2020-09-11');
+
+WITH Grps AS (
+    SELECT name, SUM(amount) AS balance
+    FROM Transactions
+    LEFT JOIN Users USING(account)
+    GROUP BY name
+)
+SELECT name, balance
+FROM Grps
+WHERE balance > 10000;
+
+Drop table if exists Users;
+Drop table if exists Transactions;
