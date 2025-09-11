a36fe8aa2fd885a2f7a12d198432a3a5173122be
drwslv <aasilva@gmail.com>
2025-09-11 08:20:06 -0700

    1873 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -1524,3 +1524,34 @@ GROUP BY event_day, emp_id;
 Drop table if exists Employees;
 
 
+/* 1873. Calculate Special Bonus [E]
+Write a solution to calculate the bonus of each employee. The bonus of an employee is 100% of their salary
+if the ID of the employee is an odd number and the employee's name does not start with the character 'M'.
+The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+*/
+
+Drop table if exists Employees;
+Create table If Not Exists Employees (employee_id int, name varchar(30), salary int);
+Truncate table Employees;
+insert into Employees (employee_id, name, salary) values ('2', 'Meir', '3000');
+insert into Employees (employee_id, name, salary) values ('3', 'Michael', '3800');
+insert into Employees (employee_id, name, salary) values ('7', 'Addilyn', '7400');
+insert into Employees (employee_id, name, salary) values ('8', 'Juan', '6100');
+insert into Employees (employee_id, name, salary) values ('9', 'Kannon', '7700');
+
+SELECT *
+FROM Employees;
+
+SELECT employee_id,
+    CASE
+        WHEN employee_id%2 = 1 AND name NOT LIKE 'M%' THEN salary
+        ELSE 0
+    END AS bonus
+FROM Employees
+ORDER BY employee_id;
+
+Drop table if exists Employees;
+
+
