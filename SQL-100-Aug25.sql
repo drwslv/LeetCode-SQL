aef4f0a98749478b80723c7a65bade01173024fa
drwslv <aasilva@gmail.com>
2025-09-11 08:12:53 -0700

    1741 [E]

--- SQL-100-Aug25.sql
+++ SQL-100-Aug25.sql
@@ -1490,7 +1490,37 @@ FROM CTE
 WHERE missing = 1
 ORDER BY employee_id ASC;
 
-
-
 Drop table if exists Employees;
 Drop table if exists Salaries;
+
+
+/* 1741. Find Total Time Spent by Each Employee [E]
+Write a solution to calculate the total time in minutes spent by each employee on each day at the office.
+Note that within one day, an employee can enter and leave more than once. The time spent in the office for a single entry is out_time - in_time.
+
+Return the result table in any order.
+*/
+
+Drop table if exists Employees;
+Create table If Not Exists Employees(emp_id int, event_day date, in_time int, out_time int);
+Truncate table Employees;
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-11-28', '4', '32');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-11-28', '55', '200');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('1', '2020-12-3', '1', '42');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('2', '2020-11-28', '3', '33');
+insert into Employees (emp_id, event_day, in_time, out_time) values ('2', '2020-12-9', '47', '74');
+
+SELECT *
+FROM Employees;
+
+WITH CTE AS (
+    SELECT *, out_time - in_time AS total_time
+    FROM Employees
+)
+SELECT event_day AS day, emp_id, SUM(total_time) AS total_time
+FROM CTE
+GROUP BY event_day, emp_id;
+
+Drop table if exists Employees;
+
+
