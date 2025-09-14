3a34de57eccae44208dd88b1bdb055dc68707c6c
drwslv <aasilva@gmail.com>
2025-09-14 11:22:10 -0700

    175 [E]

---
+++ SQL-317.sql
@@ -0,0 +1,31 @@
+
+
+/* 175. Combine Two Tables [E]
+Write a solution to report the first name, last name, city, and state of each person in the Person table.
+If the address of a personId is not present in the Address table, report null instead.
+Return the result table in any order.
+*/
+
+Drop table if exists Person;
+Drop table if exists Address;
+Create table If Not Exists Person (personId int, firstName varchar(255), lastName varchar(255));
+Create table If Not Exists Address (addressId int, personId int, city varchar(255), state varchar(255));
+Truncate table Person;
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen');
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob');
+Truncate table Address;
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York');
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California');
+
+SELECT *
+FROM Person;
+
+SELECT *
+FROM Address;
+
+SELECT firstName, lastName, city, state
+FROM Person
+LEFT JOIN Address
+    USING(personId);
+
+
