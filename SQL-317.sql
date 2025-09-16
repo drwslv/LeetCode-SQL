c0a167f7690c5c1423fe0499b5043eb704ec8035
drwslv <aasilva@gmail.com>
2025-09-16 09:32:54 -0700

    197 [E]

--- SQL-317.sql
+++ SQL-317.sql
@@ -141,3 +141,29 @@ SELECT E.name
 FROM Grp2
 JOIN Employee E
     ON Grp2.managerId = E.id;
+
+
+/* 197. Rising Temperature [E]
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+Return the result table in any order.
+*/
+
+Drop table if exists Weather;
+Create table If Not Exists Weather (id int, recordDate date, temperature int);
+Truncate table Weather;
+insert into Weather (id, recordDate, temperature) values ('1', '2015-01-01', '10');
+insert into Weather (id, recordDate, temperature) values ('2', '2015-01-02', '25');
+insert into Weather (id, recordDate, temperature) values ('3', '2015-01-03', '20');
+insert into Weather (id, recordDate, temperature) values ('4', '2015-01-04', '30');
+
+WITH WT AS (
+    SELECT *, DATE_SUB(recordDate, INTERVAL 1 DAY) AS yesterdayDate
+    FROM Weather
+)
+SELECT WT.id AS id
+FROM WT
+LEFT JOIN Weather WY
+    ON WT.yesterdayDate = WY.recordDate
+WHERE WT.temperature > WY.temperature;
+
+
