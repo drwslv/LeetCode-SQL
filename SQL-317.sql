be61a070a8ca969bad87d4724c8f06aad33f9c0b
drwslv <aasilva@gmail.com>
2025-09-17 09:18:41 -0700

    178 [M]

--- SQL-317.sql
+++ SQL-317.sql
@@ -446,3 +446,27 @@ JOIN Person P2
     AND P1.Id > P2.Id;
 
 
+/* 178. Rank Scores [M]
+Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
+-The scores should be ranked from the highest to the lowest.
+-If there is a tie between two scores, both should have the same ranking.
+-After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+*/
+
+Drop table if exists Scores;
+Create table If Not Exists Scores (id int, score DECIMAL(3,2));
+Truncate table Scores;
+insert into Scores (id, score) values ('1', '3.5');
+insert into Scores (id, score) values ('2', '3.65');
+insert into Scores (id, score) values ('3', '4.0');
+insert into Scores (id, score) values ('4', '3.85');
+insert into Scores (id, score) values ('5', '4.0');
+insert into Scores (id, score) values ('6', '3.65');
+
+SELECT *
+FROM Scores;
+
+SELECT score, DENSE_RANK() OVER(ORDER BY score DESC) AS 'rank'
+FROM Scores;
+
