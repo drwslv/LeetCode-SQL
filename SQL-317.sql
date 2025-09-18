9d46afded1c9d5b34e62aa018adb3626de9214e3
drwslv <aasilva@gmail.com>
2025-09-18 08:56:30 -0700

    184 [M]

--- SQL-317.sql
+++ SQL-317.sql
@@ -539,3 +539,42 @@ WHERE E1.primary_flag = 'Y'
     OR E2.num_dept = 1;
 
 
+/* 184. Department Highest Salary [M]
+Write a solution to find employees who have the highest salary in each of the departments.
+Return the result table in any order.
+*/
+
+Drop table if exists Employee;
+Drop table if exists Department;
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int);
+Create table If Not Exists Department (id int, name varchar(255));
+Truncate table Employee;
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '70000', '1');
+insert into Employee (id, name, salary, departmentId) values ('2', 'Jim', '90000', '1');
+insert into Employee (id, name, salary, departmentId) values ('3', 'Henry', '80000', '2');
+insert into Employee (id, name, salary, departmentId) values ('4', 'Sam', '60000', '2');
+insert into Employee (id, name, salary, departmentId) values ('5', 'Max', '90000', '1');
+Truncate table Department;
+insert into Department (id, name) values ('1', 'IT');
+insert into Department (id, name) values ('2', 'Sales');
+
+SELECT *
+FROM Employee;
+
+SELECT *
+FROM Department;
+
+WITH High AS (
+    SELECT departmentId, MAX(salary) as max_sal
+    FROM Employee
+    GROUP BY departmentId
+)
+SELECT DISTINCT D.name AS Department, E.name AS Employee, E.salary AS Salary
+FROM Employee E
+LEFT JOIN Department D
+    ON E.departmentId = D.id
+JOIN High H
+    ON E.salary = H.max_sal
+    AND E.departmentId = H.departmentId;
+
+
