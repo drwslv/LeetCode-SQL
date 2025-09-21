31740abb360b857a71562cf8d6a388d3832c1ad5
drwslv <aasilva@gmail.com>
2025-09-21 09:28:24 -0700

    1280 [E]

--- SQL-317.sql
+++ SQL-317.sql
@@ -718,4 +718,59 @@ LEFT JOIN Department D
     ON E.departmentId = D.id;
 
 
+/* 1280. Students and Examinations [E]
+Write a solution to find the number of times each student attended each exam.
+Return the result table ordered by student_id and subject_name.
+*/
+
+Drop table if exists Students;
+Drop table if exists Subjects;
+Drop table if exists Examinations;
+Create table If Not Exists Students (student_id int, student_name varchar(20));
+Create table If Not Exists Subjects (subject_name varchar(20));
+Create table If Not Exists Examinations (student_id int, subject_name varchar(20));
+Truncate table Students;
+insert into Students (student_id, student_name) values ('1', 'Alice');
+insert into Students (student_id, student_name) values ('2', 'Bob');
+insert into Students (student_id, student_name) values ('13', 'John');
+insert into Students (student_id, student_name) values ('6', 'Alex');
+Truncate table Subjects;
+insert into Subjects (subject_name) values ('Math');
+insert into Subjects (subject_name) values ('Physics');
+insert into Subjects (subject_name) values ('Programming');
+Truncate table Examinations;
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Programming');
+insert into Examinations (student_id, subject_name) values ('2', 'Programming');
+insert into Examinations (student_id, subject_name) values ('1', 'Physics');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Math');
+insert into Examinations (student_id, subject_name) values ('13', 'Programming');
+insert into Examinations (student_id, subject_name) values ('13', 'Physics');
+insert into Examinations (student_id, subject_name) values ('2', 'Math');
+insert into Examinations (student_id, subject_name) values ('1', 'Math');
+
+SELECT *
+FROM Students;
+
+SELECT *
+FROM Subjects;
+
+SELECT *
+FROM Examinations;
+
+WITH Grp AS (
+    SELECT student_id, subject_name, COUNT(*) as attended_exams
+    FROM Examinations
+    GROUP BY student_id, subject_name
+),
+StEx AS (
+    SELECT *
+    FROM Students, Subjects
+)
+SELECT S.student_id, S.student_name, S.subject_name, IFNULL(G.attended_exams,0) AS attended_exams
+FROM StEx S
+LEFT JOIN Grp G USING(student_id, subject_name, student_id)
+ORDER BY student_id, subject_name;
 
