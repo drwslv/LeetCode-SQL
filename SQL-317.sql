8fac0bdd9c8ba242e09c0ff04274f1f5c96b0f84
drwslv <aasilva@gmail.com>
2025-09-21 08:36:35 -0700

    185 [H]

--- SQL-317.sql
+++ SQL-317.sql
@@ -672,4 +672,50 @@ SELECT *, IF(x + y + z - 2 * GREATEST(x, y, z) > 0, 'Yes', 'No') AS triangle
 FROM Triangle;
 
 
+/*  185. Department Top Three Salaries [H]
+A company's executives are interested in seeing who earns the most money in each of the company's departments.
+A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+Write a solution to find the employees who are high earners in each of the departments.
+Return the result table in any order.
+*/
+
+Drop table if exists Employee;
+Drop table if exists Department;
+Create table If Not Exists Employee (id int, name varchar(255), salary int, departmentId int);
+Create table If Not Exists Department (id int, name varchar(255));
+Truncate table Employee;
+insert into Employee (id, name, salary, departmentId) values ('1', 'Joe', '85000', '1');
+insert into Employee (id, name, salary, departmentId) values ('2', 'Henry', '80000', '2');
+insert into Employee (id, name, salary, departmentId) values ('3', 'Sam', '60000', '2');
+insert into Employee (id, name, salary, departmentId) values ('4', 'Max', '90000', '1');
+insert into Employee (id, name, salary, departmentId) values ('5', 'Janet', '69000', '1');
+insert into Employee (id, name, salary, departmentId) values ('6', 'Randy', '85000', '1');
+insert into Employee (id, name, salary, departmentId) values ('7', 'Will', '70000', '1');
+Truncate table Department;
+insert into Department (id, name) values ('1', 'IT');
+insert into Department (id, name) values ('2', 'Sales');
+
+SELECT *
+FROM Employee;
+
+SELECT *
+FROM Department;
+
+WITH CTE AS (
+    SELECT DISTINCT departmentId, salary,
+        DENSE_RANK() OVER(PARTITION BY departmentId ORDER BY salary DESC) AS salary_rank
+    FROM Employee
+),
+    CTE2 AS (
+    SELECT *
+    FROM CTE
+    WHERE salary_rank <= 3
+)
+SELECT D.name AS Department, E.name AS Employee, E.salary as Salary
+FROM Employee E
+JOIN CTE2 C USING(departmentId, salary)
+LEFT JOIN Department D
+    ON E.departmentId = D.id;
+
+
 
