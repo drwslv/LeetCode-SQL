075c8087bad3211856b9bacb53087f3c008f2f59
drwslv <aasilva@gmail.com>
2025-09-24 07:54:28 -0700

    577 [E]

--- SQL-317.sql
+++ SQL-317.sql
@@ -801,4 +801,29 @@ WHERE id NOT IN (
 );
 
 
+/* 577. Employee Bonus [E]
+Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+Return the result table in any order.
+*/
+
+Drop table if exists Employee;
+Drop table if exists Bonus;
+Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int);
+Create table If Not Exists Bonus (empId int, bonus int);
+Truncate table Employee;
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', NULL, '4000');
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000');
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000');
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000');
+Truncate table Bonus;
+insert into Bonus (empId, bonus) values ('2', '500');
+insert into Bonus (empId, bonus) values ('4', '2000');
+
+SELECT name, bonus
+FROM Employee
+LEFT JOIN Bonus
+    USING(empId)
+WHERE bonus IS NULL
+    OR bonus < 1000;
+
 
