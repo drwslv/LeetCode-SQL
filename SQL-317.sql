21549fe0b26b45db4e80a890c0a94abfd6dbbfb6
drwslv <aasilva@gmail.com>
2025-09-25 10:27:36 -0700

    1378 [E]

--- SQL-317.sql
+++ SQL-317.sql
@@ -921,3 +921,31 @@ GROUP BY id
 ORDER BY COUNT(*) DESC
 LIMIT 1;
 
+
+/* 1378. Replace Employee ID With The Unique Identifier [E]
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+Return the result table in any order.
+*/
+
+Drop table if exists Employees;
+Drop table if exists EmployeeUNI;
+Create table If Not Exists Employees (id int, name varchar(20));
+Create table If Not Exists EmployeeUNI (id int, unique_id int);
+Truncate table Employees;
+insert into Employees (id, name) values ('1', 'Alice');
+insert into Employees (id, name) values ('7', 'Bob');
+insert into Employees (id, name) values ('11', 'Meir');
+insert into Employees (id, name) values ('90', 'Winston');
+insert into Employees (id, name) values ('3', 'Jonathan');
+Truncate table EmployeeUNI;
+insert into EmployeeUNI (id, unique_id) values ('3', '1');
+insert into EmployeeUNI (id, unique_id) values ('11', '2');
+insert into EmployeeUNI (id, unique_id) values ('90', '3');
+
+SELECT unique_id, name
+FROM Employees E
+LEFT JOIN EmployeeUNI U
+    USING(id);
+
+
+
