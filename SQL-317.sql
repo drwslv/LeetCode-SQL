d852adce6a83d9451e97633dbbf69f9bd7950651
drwslv <aasilva@gmail.com>
2025-09-25 10:23:02 -0700

    602 [M]

--- SQL-317.sql
+++ SQL-317.sql
@@ -894,3 +894,30 @@ WITH CTE AS (
 SELECT ROUND(100*SUM(first_order = first_pref)/COUNT(*),2) AS immediate_percentage
 FROM CTE;
 
+
+/* 602. Friend Requests II: Who Has the Most Friends [M]
+Write a solution to find the people who have the most friends and the most friends number.
+The test cases are generated so that only one person has the most friends.
+*/
+
+Drop table if exists RequestAccepted;
+Create table If Not Exists RequestAccepted (requester_id int not null, accepter_id int null, accept_date date null);
+Truncate table RequestAccepted;
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '2', '2016/06/03');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '3', '2016/06/08');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('2', '3', '2016/06/08');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('3', '4', '2016/06/09');
+
+WITH CTE AS (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted
+)
+SELECT id, COUNT(*) AS num
+FROM CTE
+GROUP BY id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
